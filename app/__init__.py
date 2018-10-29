5c31f6f2caac0ec451e3e9eee4f5a1e4901bad6d
Alan Quin NG Martinez <AlanQ.NG@cz.abb.com>
2018-10-29 14:31:01 +0100

    Add Google Oauth

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_dance.contrib.google import make_google_blueprint, google
+from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
 
-app = Flask('__name__')
+google_blueprint = make_google_blueprint(client_id='35189606116-d1pge95126e7vmllgvc69v8ihmr5p9cl.apps.googleusercontent.com', client_secret='zY4T7G2Xp__FmRPhAEd_YKDE',)
+
+
+app = Flask(__name__)
+app.register_blueprint(google_blueprint, url_prefix='/login')
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///items.db"
+app.config['SECRET_KEY'] = 'thisissupersecret'
+
 db = SQLAlchemy(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,17 @@
-from app import app
+from app import app, google_blueprint, google
 from flask import url_for, redirect, render_template
+from flask_dance.contrib.google import google
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('test.html')
+    return render_template('base.html')
 
-@app.route("/login")
+@app.route('/login')
 def login():
-    pass
+    if not google.authorized:
+        return redirect(url_for('google.login'))
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    return "You are {email} on Google".format(email=resp.json()["email"])
+    
